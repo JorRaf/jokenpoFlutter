99bec2f830761387e16daa95bb94e42042abf03c
JorRaf <jorrafoliver@gmail.com>
2020-03-24 16:05:18 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:jokenpo/Jogo.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(App());
+
+
+/*void main(){
+  runApp(MaterialApp(
+    home:Jogo(),
+    debugShowCheckedModeBanner: false,
+  ));
+}*/
+
+class App extends StatelessWidget{
+  static const String _title = 'Joken Po';
+
+  @override
+  Widget build(BuildContext context) {
+
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
+    // TODO: implement build
+    return MaterialApp(
+      title: _title,
+      home: Jogo(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
